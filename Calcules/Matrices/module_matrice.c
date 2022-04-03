a8d372c771f211b77404ac03f43826c2c1ee4fb9
ThomasCreuset <95984752+ThomasCreuset@users.noreply.github.com>
2022-04-03 16:51:16 +0200

    Add files via upload

--- Calcules/Matrices/module_matrice.c
+++ Calcules/Matrices/module_matrice.c
@@ -3,17 +3,14 @@
 #include "module_matrice.h"
 
 
-/*  -Constantes-  */
-
-const valeur DEFAULT = 0;
-
-
 /* -Fonctions- */
 
 matrice* creer_matrice(int lignes, int colonnes) {
     if (lignes <= 0 || colonnes <= 0) {
         fprintf(stderr, "création impossible:\n");
-        fprintf(stderr, "\t-> les dimensions doivent-être des entiers non nuls.");
+        fprintf(stderr, "\t-> les tailles 'lignes' et 'colonnes' doivent-être des entiers non nuls.\n");
+        fprintf(stderr, "\t\t'lignes': {%d}\n", lignes);
+        fprintf(stderr, "\t\t'colonnes': {%d}\n", colonnes);
         exit(EXIT_FAILURE);
     }
     matrice* matriceSortie  = malloc(sizeof(matrice));
@@ -23,7 +20,7 @@ matrice* creer_matrice(int lignes, int colonnes) {
     for (int ligne = 0; ligne < lignes; ++ligne) {
         matriceSortie->contenu[ligne] = malloc(sizeof(valeur)*colonnes);
         for (int colonne = 0; colonne < colonnes; ++colonne) {
-            matriceSortie->contenu[ligne][colonne] = DEFAULT;
+            matriceSortie->contenu[ligne][colonne] = 0;
         }
     }
     return matriceSortie;
@@ -40,7 +37,11 @@ void supprimer_matrice(matrice* matriceEntree) {
 matrice* add_matrice(matrice* matriceA, matrice* matriceB) {
     if (matriceA->lignes != matriceB->lignes || matriceA->colonnes != matriceB->colonnes) {
         fprintf(stderr, "addition impossible:\n");
-        fprintf(stderr, "\t-> les deux matrices doivent-être de taille identique.");
+        fprintf(stderr, "\t-> les deux matrices doivent-être de taille identique.\n");
+        fprintf(stderr, "\t\t'matriceA': {%d} lignes\n", matriceA->lignes);
+        fprintf(stderr, "\t\t'matriceA': {%d} colonnes\n", matriceA->colonnes);
+        fprintf(stderr, "\t\t'matriceB': {%d} lignes\n", matriceB->lignes);
+        fprintf(stderr, "\t\t'matriceB': {%d} colonnes\n", matriceB->colonnes);
         exit(EXIT_FAILURE);
     }
     matrice* matriceSortie = creer_matrice(matriceA->lignes, matriceA->colonnes);
@@ -55,7 +56,11 @@ matrice* add_matrice(matrice* matriceA, matrice* matriceB) {
 matrice* soustract_matrice(matrice* matriceA, matrice* matriceB) {
     if (matriceA->lignes != matriceB->lignes || matriceA->colonnes != matriceB->colonnes) {
         fprintf(stderr, "soustraction impossible:\n");
-        fprintf(stderr, "\t-> les deux matrices doivent-être de taille identique.");
+        fprintf(stderr, "\t-> les deux matrices doivent-être de taille identique.\n");
+        fprintf(stderr, "\t\t'matriceA': {%d} lignes\n", matriceA->lignes);
+        fprintf(stderr, "\t\t'matriceA': {%d} colonnes\n", matriceA->colonnes);
+        fprintf(stderr, "\t\t'matriceB': {%d} lignes\n", matriceB->lignes);
+        fprintf(stderr, "\t\t'matriceB': {%d} colonnes\n", matriceB->colonnes);
         exit(EXIT_FAILURE);
     }
     matrice* matriceSortie = creer_matrice(matriceA->lignes, matriceA->colonnes);
@@ -70,14 +75,16 @@ matrice* soustract_matrice(matrice* matriceA, matrice* matriceB) {
 matrice* mult_matrice(matrice* matriceA, matrice* matriceB) {
     if (matriceA->colonnes != matriceB->lignes) {
         fprintf(stderr, "multiplication impossible:\n");
-        fprintf(stderr, "\t-> la matrice 'matriceA' doit avoir autant de colonnes que la matrice 'matriceB' a de lignes.");
+        fprintf(stderr, "\t-> la matrice 'matriceA' doit avoir autant de colonnes que la matrice 'matriceB' a de lignes.\n");
+        fprintf(stderr, "\t\t'matriceA': {%d} colonnes\n", matriceA->colonnes);
+        fprintf(stderr, "\t\t'matriceB': {%d} lignes\n", matriceB->lignes);
         exit(EXIT_FAILURE);
     }
     int tailleCommune = matriceA->colonnes;
     matrice* matriceSortie = creer_matrice(matriceA->lignes, matriceB->colonnes);
     for (int ligne = 0; ligne < matriceSortie->lignes; ++ligne) {
         for (int colonne = 0; colonne < matriceSortie->colonnes; ++colonne) {
-            int somme = 0;
+            valeur somme = 0;
             for (int k = 0; k < tailleCommune; ++k) {
                 somme += matriceA->contenu[ligne][k] * matriceB->contenu[k][colonne];
             }
@@ -97,7 +104,7 @@ matrice* transp_matrice(matrice* matriceEntree) {
     return matriceSortie;
 }
 
-matrice* dilatation_matrice(int scalaire, matrice* matriceEntree) {
+matrice* dilatation_matrice(valeur scalaire, matrice* matriceEntree) {
     matrice* matriceSortie = creer_matrice(matriceEntree->lignes, matriceEntree->colonnes);
     for (int ligne = 0; ligne < matriceSortie->lignes; ++ligne) {
         for (int colonne = 0; colonne < matriceSortie->colonnes; ++colonne) {
@@ -105,4 +112,240 @@ matrice* dilatation_matrice(int scalaire, matrice* matriceEntree) {
         }
     }
     return matriceSortie;
+}
+
+valeur det_matrice(matrice* matriceEntree) {
+    if (matriceEntree->colonnes != matriceEntree->lignes) {
+        fprintf(stderr, "calcule du déterminant impossible:\n");
+        fprintf(stderr, "\t-> la matrice 'matriceEntree' doit avoir autant de colonnes que de lignes.\n");
+        fprintf(stderr, "\t\t'matriceEntree': {%d} lignes\n", matriceEntree->lignes);
+        fprintf(stderr, "\t\t'matriceEntree': {%d} colonnes\n", matriceEntree->colonnes);
+        exit(EXIT_FAILURE);
+    }
+    int tailleCommune = matriceEntree->colonnes;
+    // cas d'arrêt
+    if (tailleCommune == 1) {
+        return matriceEntree->contenu[0][0];
+    }
+    // recherche meilleur ligne/colonne (celle possèdant le moins de 0)
+    int idMeilleur   = 0;
+    int nbreZeroMax  = 0;
+    bool estVertical = false;
+    for (int ligne  = 0; ligne < tailleCommune; ++ligne) {
+        int nbreZero = 0;
+        for (int colonne = 0; colonne < tailleCommune; ++colonne) {
+            if (matriceEntree->contenu[ligne][colonne] == 0) {
+                nbreZero += 1;
+            }
+        }
+        if (nbreZero > nbreZeroMax) {
+            idMeilleur = ligne;
+        }
+    }
+    for (int colonne  = 0; colonne < tailleCommune; ++colonne) {
+        int nbreZero = 0;
+        for (int ligne = 0; ligne < tailleCommune; ++ligne) {
+            if (matriceEntree->contenu[ligne][colonne] == 0) {
+                nbreZero += 1;
+            }
+        }
+        if (nbreZero > nbreZeroMax) {
+            idMeilleur  = colonne;
+            estVertical = true;
+        }
+    }
+    // calcule du déterminant (on se ramène à la transoposé si le calcule le plus intéressant est sur une colonne)
+    valeur det = 0;
+    if (estVertical) {
+        matriceEntree = transp_matrice(matriceEntree);
+    }
+    matrice* matriceTemp = creer_matrice(tailleCommune-1, tailleCommune-1);
+    for (int colonneEnCours = 0; colonneEnCours < tailleCommune; ++colonneEnCours) {
+        if (matriceEntree->contenu[idMeilleur][colonneEnCours] != 0) {
+            int ligneTemp = 0;
+            int colonneTemp = 0;
+            for (int ligne = 0; ligne < tailleCommune; ++ligne) {
+                for(int colonne = 0; colonne < tailleCommune; ++colonne) {
+                    if (ligne != idMeilleur && colonne != colonneEnCours) {
+                        matriceTemp->contenu[ligneTemp][colonneTemp] = matriceEntree->contenu[ligne][colonne];
+                        colonneTemp += 1;
+                    }
+                }
+                if (ligne != idMeilleur) {
+                    ligneTemp += 1;
+                }
+            }
+            det += matriceEntree->contenu[idMeilleur][colonneEnCours] * det_matrice(matriceTemp);
+        }
+    }
+    supprimer_matrice(matriceTemp);
+    return det;
+}
+
+void echange_ligne(matrice* matriceEntree, int ligne1, int ligne2) {
+    if (ligne1 >= matriceEntree->lignes || ligne1 < 0 || ligne2 >= matriceEntree->lignes || ligne2 < 0) {
+        fprintf(stderr, "échange des lignes impossible:\n");
+        fprintf(stderr, "\t-> les lignes 'ligne1' et 'ligne2' doivent exister.\n");
+        fprintf(stderr, "\t\t'ligne1': {%d}\n", ligne1);
+        fprintf(stderr, "\t\t'ligne2': {%d}\n", ligne2);
+        exit(EXIT_FAILURE);
+    }
+    for (int colonne = 0; colonne < matriceEntree->colonnes; ++colonne) {
+        valeur stockageTemp                     = matriceEntree->contenu[ligne1][colonne];
+        matriceEntree->contenu[ligne1][colonne] = matriceEntree->contenu[ligne2][colonne];
+        matriceEntree->contenu[ligne2][colonne] = stockageTemp;
+    }
+}
+
+void echange_colonne(matrice* matriceEntree, int colonne1, int colonne2) {
+    if (colonne1 >= matriceEntree->colonnes || colonne1 < 0 || colonne2 >= matriceEntree->colonnes || colonne2 < 0) {
+        fprintf(stderr, "échange des colonnes impossible:\n");
+        fprintf(stderr, "\t-> les colonnes 'colonne1' et 'colonne2' doivent exister.\n");
+        fprintf(stderr, "\t\t'colonne1': {%d}\n", colonne1);
+        fprintf(stderr, "\t\t'colonne2': {%d}\n", colonne2);
+        exit(EXIT_FAILURE);
+    }
+    for (int ligne = 0; ligne < matriceEntree->lignes; ++ligne) {
+        valeur stockageTemp                     = matriceEntree->contenu[ligne][colonne1];
+        matriceEntree->contenu[ligne][colonne1] = matriceEntree->contenu[ligne][colonne2];
+        matriceEntree->contenu[ligne][colonne2] = stockageTemp;
+    }
+}
+
+void combinaison_lignes(matrice* matriceEntree, int ligneDest, valeur scalaire, int ligneAjout) {
+    if (ligneDest >= matriceEntree->lignes || ligneDest < 0 || ligneAjout >= matriceEntree->lignes || ligneAjout < 0) {
+        fprintf(stderr, "combinaison des lignes impossible:\n");
+        fprintf(stderr, "\t-> les lignes 'ligneDest' et 'ligneAjout' doivent exister.\n");
+        fprintf(stderr, "\t\t'ligneDest': {%d}\n", ligneDest);
+        fprintf(stderr, "\t\t'ligneAjout': {%d}\n", ligneAjout);
+        exit(EXIT_FAILURE);
+    }
+    for (int colonne = 0; colonne < matriceEntree->colonnes; ++colonne) {
+        matriceEntree->contenu[ligneDest][colonne] = matriceEntree->contenu[ligneDest][colonne] + scalaire * matriceEntree->contenu[ligneAjout][colonne];
+    }
+}
+
+void combinaison_colonne(matrice* matriceEntree, int colonneDest, valeur scalaire, int colonneAjout) {
+    if (colonneDest >= matriceEntree->colonnes || colonneDest < 0 || colonneAjout >= matriceEntree->colonnes || colonneAjout < 0) {
+        fprintf(stderr, "combinaison des colonnes impossible:\n");
+        fprintf(stderr, "\t-> les colonnes 'colonneDest' et 'colonneAjout' doivent exister.\n");
+        fprintf(stderr, "\t\t'colonneDest': {%d}\n", colonneDest);
+        fprintf(stderr, "\t\t'colonneAjout': {%d}\n", colonneAjout);
+        exit(EXIT_FAILURE);
+    }
+    for (int ligne = 0; ligne < matriceEntree->lignes; ++ligne) {
+        matriceEntree->contenu[ligne][colonneDest] = matriceEntree->contenu[ligne][colonneDest] + scalaire * matriceEntree->contenu[ligne][colonneAjout];
+    }
+}
+
+void dilatation_ligne(matrice* matriceEntree, valeur scalaire, int ligne) {
+    if (ligne >= matriceEntree->lignes || ligne < 0) {
+        fprintf(stderr, "dilatation de la ligne impossible:\n");
+        fprintf(stderr, "\t-> la ligne 'ligne' doit exister.\n");
+        fprintf(stderr, "\t\t'ligne': {%d}\n", ligne);
+        exit(EXIT_FAILURE);
+    }
+    if (scalaire == 0) {
+        fprintf(stderr, "dilatation de la ligne impossible:\n");
+        fprintf(stderr, "\t-> le scalaire 'scalaire' doit être non nul.\n");
+        fprintf(stderr, "\t\t'scalaire': {%f}\n", scalaire); // à modifier si valeur change de type
+        exit(EXIT_FAILURE);
+    }
+    for (int colonne = 0; colonne < matriceEntree->colonnes; ++colonne) {
+        matriceEntree->contenu[ligne][colonne] = scalaire * matriceEntree->contenu[ligne][colonne];
+    }
+}
+
+void dilatation_colonne(matrice* matriceEntree, valeur scalaire, int colonne) {
+    if (colonne >= matriceEntree->colonnes || colonne < 0) {
+        fprintf(stderr, "dilatation de la colonne impossible:\n");
+        fprintf(stderr, "\t-> la colonne 'colonne' doit exister.\n");
+        fprintf(stderr, "\t\t'colonne': {%d}\n", colonne);
+        exit(EXIT_FAILURE);
+    }
+    if (scalaire == 0) {
+        fprintf(stderr, "dilatation de la colonne impossible:\n");
+        fprintf(stderr, "\t-> le scalaire 'scalaire' doit être non nul.\n");
+        fprintf(stderr, "\t\t'scalaire': {%f}\n", scalaire); // à modifier si valeur change de type
+        exit(EXIT_FAILURE);
+    }
+    for (int ligne = 0; ligne < matriceEntree->lignes; ++ligne) {
+        matriceEntree->contenu[ligne][colonne] = scalaire * matriceEntree->contenu[ligne][colonne];
+    }
+}
+
+matrice* inv_matrice(matrice* matriceEntree) {
+    if (matriceEntree->colonnes != matriceEntree->lignes) {
+        fprintf(stderr, "calcule de l'inverse impossible:\n");
+        fprintf(stderr, "\t-> la matrice 'matriceEntree' doit avoir autant de colonnes que de lignes.\n");
+        fprintf(stderr, "\t\t'matriceEntree': {%d} lignes\n", matriceEntree->lignes);
+        fprintf(stderr, "\t\t'matriceEntree': {%d} colonnes\n", matriceEntree->colonnes);
+        exit(EXIT_FAILURE);
+    }
+    valeur det = det_matrice(matriceEntree);
+    if (det == 0) {
+        fprintf(stderr, "calcule de l'inverse impossible:\n");
+        fprintf(stderr, "\t-> la matrice 'matriceEntree' est de déterminant nul.\n");
+        fprintf(stderr, "\t\tdéterminant: {%f}\n", det); // à modifier si valeur change de type
+        exit(EXIT_FAILURE);
+        
+    }
+    // mise en place
+    int tailleCommune = matriceEntree->colonnes;
+    matrice* matriceTemp = creer_matrice(tailleCommune, 2*tailleCommune);
+    for (int ligne = 0; ligne < tailleCommune; ++ligne) {
+        for (int colonne = 0; colonne < tailleCommune; ++ colonne) {
+            matriceTemp->contenu[ligne][colonne] = matriceEntree->contenu[ligne][colonne];
+            if (ligne == colonne) {
+                matriceTemp->contenu[ligne][colonne+tailleCommune] = 1;
+            }
+            else {
+                matriceTemp->contenu[ligne][colonne+tailleCommune] = 0;
+            }
+        }
+    }
+    // algorithme de Gauss-Jordan
+    int lignePivot   = -1;
+    for (int colonne = 0; colonne < tailleCommune; ++colonne) {
+        int ligneMax = lignePivot+1;
+        int maximum  = matriceTemp->contenu[lignePivot+1][colonne];
+        for (int ligne = lignePivot+2; ligne < tailleCommune; ++ligne) {
+            if (matriceTemp->contenu[ligne][colonne] > maximum) {
+                maximum = matriceTemp->contenu[ligne][colonne];
+                ligneMax = ligne;
+            }
+        }
+        if (matriceTemp->contenu[ligneMax][colonne] != 0) {
+            lignePivot += 1;
+            dilatation_ligne(matriceTemp, 1/matriceTemp->contenu[ligneMax][colonne], ligneMax);
+            if (ligneMax != lignePivot) {
+                echange_ligne(matriceTemp, lignePivot, ligneMax);
+            }
+            for (int ligne = 0; ligne < tailleCommune; ++ligne) {
+                if (ligne != lignePivot) {
+                    combinaison_lignes(matriceTemp, ligne, (-1)*matriceTemp->contenu[ligne][colonne], lignePivot);
+                }
+            }
+        }
+    }
+    // recopie de la matrice inverse
+    matrice* matriceSortie = creer_matrice(tailleCommune, tailleCommune);
+    for (int ligne = 0; ligne < tailleCommune; ++ligne) {
+        for (int colonne = 0; colonne < tailleCommune; ++colonne) {
+            matriceSortie->contenu[ligne][colonne] = matriceTemp->contenu[ligne][colonne+tailleCommune];
+        }
+    }
+    supprimer_matrice(matriceTemp);
+    return matriceSortie;
+}
+
+matrice* affichage_matrice(matrice* matriceEntree) {
+    printf("Affichage:\n");
+    for (int ligne = 0; ligne < matriceEntree->lignes; ++ligne) {
+        printf("|");
+        for (int colonne = 0; colonne < matriceEntree->colonnes; ++colonne) {
+            printf(" {%f} ", matriceEntree->contenu[ligne][colonne]);
+        }
+        printf("|\n");
+    }
 }
--- Calcules/main.c
+++ Calcules/main.c
@@ -7,6 +7,24 @@
 /*  -Main-  */
 
 int main() {
-    
+    matrice* maMatrice1 = creer_matrice(2, 2);
+    matrice* maMatrice2 = creer_matrice(2, 2);
+    maMatrice1->contenu[0][0] = 3.2;
+    maMatrice1->contenu[1][0] = 5.1;
+    maMatrice1->contenu[0][1] = 1.0;
+    maMatrice1->contenu[1][1] = 2.7;
+    maMatrice2->contenu[0][0] = 9.3;
+    maMatrice2->contenu[1][0] = 12.6;
+    maMatrice2->contenu[0][1] = 0.5;
+    maMatrice2->contenu[1][1] = 2.1;
+    printf("-> affichage simple:\n");
+    affichage_matrice(maMatrice1);
+    affichage_matrice(maMatrice2);
+    printf("-> inverse:\n");
+    affichage_matrice(inv_matrice(maMatrice1));
+    printf("-> multiplication:\n");
+    affichage_matrice(mult_matrice(maMatrice1, maMatrice2));
+    printf("-> addition:\n");
+    affichage_matrice(add_matrice(maMatrice1, maMatrice2));
     return EXIT_SUCCESS;
 }
